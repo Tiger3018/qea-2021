0ff24333748604cb890be6118413bc1b455a788b
Tiger3018 <tiger3018of02@gmail.com>
2021-04-28 17:24:23 +0800

    init

---
+++ MiniBalance/CONTROL/control.h
@@ -0,0 +1,24 @@
+#ifndef __CONTROL_H
+#define __CONTROL_H
+#include "sys.h"
+  /**************************************************************************
+作者：平衡小车之家
+我的淘宝小店：http://shop114407458.taobao.com/
+**************************************************************************/
+#define PI 3.14159265
+#define ZHONGZHI 0 
+#define DIFFERENCE 100
+extern	int Balance_Pwm,Velocity_Pwm,Turn_Pwm;
+void Kinematic_Analysis(float velocity,float turn);
+int EXTI15_10_IRQHandler(void);
+void Set_Pwm(int motor_a,int motor_b,int servo);
+void Key(void);
+void Xianfu_Pwm(int amplitude);
+void Xianfu_Velocity(int amplitude_A,int amplitude_B);
+u8 Turn_Off( int voltage);
+int myabs(int a);
+int Incremental_PI_Left (int Encoder,int Target);
+int Incremental_PI_Right (int Encoder,int Target);
+void Get_RC(void);
+void  Find_CCD_Zhongzhi(void);
+#endif
---
+++ MiniBalance_HARDWARE/USARTX/usartx.h
@@ -0,0 +1,13 @@
+#ifndef __USRATX_H
+#define __USRATX_H 
+#include "sys.h"	  	
+  /**************************************************************************
+作者：平衡小车之家
+我的淘宝小店：http://shop114407458.taobao.com/
+**************************************************************************/
+void uart5_send(u8 data);
+void uart5_init(u32 bound);
+void UART5_IRQHandler(void);
+void UART_TX(void);
+#endif
+
